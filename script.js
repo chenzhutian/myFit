3975b1d56f646fbd3a82a6c7e2fd217f5da2a850
zhutian.chen <zhutian.chen.code@outlook.com>
2020-11-21 08:24:28 -0800

    first commit

---
+++ script.js
@@ -0,0 +1,91 @@
+(async () => {
+    const data = await fetch("./data.json").then((res) => res.json());
+    const nutritionDataset = await fetch("./unit.json").then((res) =>
+        res.json()
+    );
+
+    const meals = ["breakfast", "extra1", "lunch", "extra2", "dinner", "extra3"];
+    const tableHeads = [
+        "name",
+        "serving",
+        "unit",
+        "protein",
+        "fat",
+        "carb",
+        "calorles",
+    ];
+
+    const goalCalorles = 2000
+    const goalProtein = goalCalorles * 0.35 / 4
+    const goalCarb = goalCalorles * 0.45 / 4
+    const goalFat = goalCalorles * 0.2 / 9
+    // 4 : 4 : 2
+    const goal = {
+        protein: goalProtein,
+        fat: goalFat,
+        carb: goalCarb,
+        calorles: goalCalorles
+    }
+
+    const getSumDict = () => tableHeads.slice(3).reduce((o, k) => {
+        o[k] = 0
+        return o
+    }, {})
+    document.querySelector(".content").insertAdjacentHTML(
+        "afterbegin",
+        data.map((d) => {
+            const totalSumForToday = getSumDict()
+
+            return `
+            <h5>${d.date}</h5>
+            <table>
+                <tr>${tableHeads
+                    .map((th) => `<th class="${th}">${th}</th>`)
+                    .join('')}
+                </tr>
+
+            ${meals
+                    .filter((key) => d[key] && d[key].length)
+                    .map((key) => {
+
+                        const sum = getSumDict()
+
+                        return `
+                    <tr class="meal" ><td colspan="7">${key}</td></tr>
+
+                    ${d[key].map(({ name, unit }) => {
+                            const nutrition = nutritionDataset.find(u => u.name === name);
+                            Object.keys(sum).forEach(k => sum[k] += nutrition[k]* unit)
+                            Object.keys(totalSumForToday).forEach(k => totalSumForToday[k] += nutrition[k]* unit)
+
+                            return `<tr>
+                        <td>${name}</td>
+                        <td>${nutrition.serving}</td>
+                        <td>${unit}</td>
+                        ${Object.keys(sum).map(k => `<td>${(nutrition[k] * unit).toFixed(2)}</td>`).join('')}
+                      </tr>`;
+                        }).join('')}
+
+                    <tr class="meal-sum" >
+                        <td colspan="3">Total in this ${key}</td>
+                        ${Object.keys(sum).map(k => `<td>${sum[k].toFixed(2)}</td>`).join('')}
+                    </tr> 
+                `}).join("")
+                }
+
+            <tr class="day-total">
+                <td colspan="3">Total today</td>
+                ${Object.keys(goal).map(k => `<td>${totalSumForToday[k].toFixed(2)}</td>`).join('')}
+            </tr>
+            <tr class="day-goal">
+                <td colspan="3">Goal</td>
+                ${Object.keys(goal).map(k => `<td>${goal[k].toFixed(2)}</td>`).join('')}
+            </tr>
+            <tr class="day-rest">
+                <td colspan="3">Remaining</td>
+                ${Object.keys(goal).map(k => `<td>${(goal[k] - totalSumForToday[k]).toFixed(2)}</td>`).join('')}
+            </tr>
+            </table>
+        `}).join('')
+    );
+})();
